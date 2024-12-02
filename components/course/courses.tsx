6cac21631e5d7f12377ade946ad8864b0ef0d565
LilAliTech <lilalix20@gmail.com>
2024-12-02 18:44:38 +0000

    upd

--- components/course/courses.tsx
+++ components/course/courses.tsx
@@ -6,20 +6,17 @@ export const metadata = {
   description: 'Explore our popular courses in Quran and Arabic.',
 };
 
-export const revalidate = 0;
-
 export default async function Courses() {
   // Fetch products directly in the Server Component
   const products = await getProducts({ sortKey: 'TITLE', reverse: false, query: '' });
-  console.log('Fetched products:', products);
 
   // Limit the fetched products to the first 3
   const limitedProducts = products.slice(0, 3);
 
   return (
-    <div className="p-20 bg-[#f9f5e7]" style={{ backgroundImage: "url('/courses-one_bg.png')" }}>
-      <h1 className="text-4xl font-bold text-center text-[#2c2c2c] mb-8">Our Quran & Arabic Courses</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+    <div className="p-20 bg-[#fff4d7]" style={{ backgroundImage: "url('/courses-one_bg.png')" }}>
+      <h1 className="text-3xl font-bold  text-black text-center mb-6">Popular Courses</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {limitedProducts.map((course) => (
           <CourseCard
             key={course.id}
@@ -28,7 +25,7 @@ export default async function Courses() {
             price={`${course.priceRange.minVariantPrice.amount} ${course.priceRange.minVariantPrice.currencyCode}`}
             imageUrl={course.featuredImage?.url || '/placeholder.jpg'}
             productUrl={`/products/${course.id}`}
-            buttonText='Add to Cart'
+            buttonText='Learn More'
           />
         ))}
       </div>
